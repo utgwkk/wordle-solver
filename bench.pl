cdd468dd7253f882ae05cee74092c81d3443be53
utgwkk <utagawakiki@gmail.com>
2022-02-06 21:42:36 +0900

    bench

---
+++ bench.pl
@@ -0,0 +1,12 @@
+use strict;
+use warnings;
+use v5.30;
+
+for my $i (1..10000) {
+    my $result = `perl solver.pl`;
+    if ($? > 0) {
+        say -1;
+        next;
+    }
+    say scalar(split /\r?\n/, $result);
+}
