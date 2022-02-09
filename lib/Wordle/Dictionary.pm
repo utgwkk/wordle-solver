ad982138f7d4620b42e38b059e586618803ddb67
utgwkk <utagawakiki@gmail.com>
2022-02-10 00:54:03 +0900

    uniq

--- lib/Wordle/Dictionary.pm
+++ lib/Wordle/Dictionary.pm
@@ -1,12 +1,13 @@
 package Wordle::Dictionary;
 use strict;
 use warnings;
+use List::Util qw(uniq);
 
 use constant ALL_WORDS => do {
     open my $fh, '<', '/usr/share/dict/words';
     my @words = grep { /^[a-z]{5}$/ } <$fh>;
     chomp @words;
-    [ @words ];
+    [ uniq @words ];
 };
 
 1;
